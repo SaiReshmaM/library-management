f5cc956de7ca0d3faeb3fef60b7dccea53f03563
jnslsakuntala <159747958+jnslsakuntala@users.noreply.github.com>
2026-05-12 14:55:47 +0530

    book repo commited

--- backend/src/main/java/com/library/repository/BookRepository.java
+++ backend/src/main/java/com/library/repository/BookRepository.java
@@ -1,5 +1,5 @@
 package com.library.repository;
-
+  
 import com.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
