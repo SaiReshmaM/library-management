e4c3f8cb9438aafcef67a2ff05fbce3b6e114cd1
Sai Reshma Maddu <157406677+SaiReshmaM@users.noreply.github.com>
2026-05-12 14:52:50 +0530

    updatedcommit

--- backend/src/main/java/com/library/exception/BookNotAvailableException.java
+++ backend/src/main/java/com/library/exception/BookNotAvailableException.java
@@ -2,7 +2,6 @@ package com.library.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 @ResponseStatus(HttpStatus.CONFLICT)
 public class BookNotAvailableException extends RuntimeException {
     public BookNotAvailableException(String message) {
