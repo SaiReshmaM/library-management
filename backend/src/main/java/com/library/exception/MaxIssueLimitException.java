5323efc1ed5567c73bb1023af97e312490f26790
Sai Reshma Maddu <157406677+SaiReshmaM@users.noreply.github.com>
2026-05-12 14:54:00 +0530

    updatedcommit

--- backend/src/main/java/com/library/exception/MaxIssueLimitException.java
+++ backend/src/main/java/com/library/exception/MaxIssueLimitException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    
 public class MaxIssueLimitException extends RuntimeException {
     public MaxIssueLimitException(String message) {
         super(message);
