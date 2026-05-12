d0eb998e02faf4cc03687f7689bba404b15d8429
Vinaya Sri Mandalapu <157445518+vinaya3107@users.noreply.github.com>
2026-05-12 14:48:22 +0530

    dto commit

--- backend/src/main/java/com/library/dto/response/BookResponse.java
+++ backend/src/main/java/com/library/dto/response/BookResponse.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
-public class BookResponse {
+public class BookResponse {  
 
     private Long bookId;
     private String title;
